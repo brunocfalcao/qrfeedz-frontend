2adaf708114f0df412840c284a887624a9a3bd95
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2023-02-17 19:10:26 +0100

    Initial commit

---
+++ src/FrontendServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace QRFeedz\Frontend;
+
+use Illuminate\Support\ServiceProvider;
+
+class FrontendServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        //
+    }
+
+    public function register()
+    {
+        //
+    }
+}
