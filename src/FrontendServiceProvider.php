5e10b05771e079121b1957238515ce6aa074cbed
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2023-02-19 17:31:27 +0100

    wip

--- src/FrontendServiceProvider.php
+++ src/FrontendServiceProvider.php
@@ -3,16 +3,37 @@
 namespace QRFeedz\Frontend;
 
 use Illuminate\Support\ServiceProvider;
+use QRFeedz\Frontend\Commands\Install;
 
 class FrontendServiceProvider extends ServiceProvider
 {
-    public function boot()
+    public function boot(): void
+    {
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'qrfeedz');
+
+        if (! $this->app->runningInConsole()) {
+            $this->loadRoutes();
+            $this->registerBladeComponents();
+        }
+    }
+
+    public function register(): void
     {
         //
     }
 
-    public function register()
+    protected function loadRoutes()
     {
-        //
+        $routesPath = __DIR__.'/../routes/frontend.php';
+
+        Route::middleware([
+            'web',
+            IpTracing::class,
+            VisitTracing::class,
+            GoalsTracing::class,
+        ])
+         ->group(function () use ($routesPath) {
+             include $routesPath;
+         });
     }
 }
