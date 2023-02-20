8e79a28a531968dbd3a8aa79b84ca99dcd93efa9
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2023-02-20 22:08:43 +0100

    Update InstanceController.php

--- src/Controllers/InstanceController.php
+++ src/Controllers/InstanceController.php
@@ -3,9 +3,26 @@
 namespace QRFeedz\Frontend\Controllers;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\App;
 
 class InstanceController extends Controller
 {
+    /**
+     * Instantiate a new controller instance.
+     */
+    public function __construct()
+    {
+        if (! App::environment(['local', 'staging'])) {
+            $this->middleware('throttle:15,1')->only('new');
+        }
+    }
+
+    /**
+     * The instance is triggered when a new questionnaire is requested.
+     *
+     * @param  string  $uuid The qrcode id
+     * @return mixed
+     */
     public function new(string $uuid)
     {
         return view('qrfeedz::instance');
