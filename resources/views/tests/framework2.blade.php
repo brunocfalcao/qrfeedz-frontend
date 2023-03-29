9e444c199ce8b52e985e55fdf0f9cc3e97d02469
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2023-03-29 15:44:18 +0200

    Update framework2.blade.php

--- resources/views/tests/framework2.blade.php
+++ resources/views/tests/framework2.blade.php
@@ -6,10 +6,16 @@
     <title>Version 3</title>
     <!-- Include Tailwind CSS -->
     <script src="https://cdn.tailwindcss.com"></script>
+    <style>
+        /* Add some padding to the bottom of the body to avoid overlap with the bottom toolbar */
+        body {
+            padding-bottom: env(safe-area-inset-bottom);
+        }
+    </style>
   </head>
-<body class="flex justify-center items-center h-screen border-4 border-red-500">
+  <body class="flex justify-center items-center h-screen border-4 border-red-500">
     <div class="bg-red-500 rounded-lg p-8">
-      <h1 class="text-white text-center text-4xl">Hello, world!</h1>
+    <h1 class="text-white text-center text-4xl">Hello, world!</h1>
     </div>
   </body>
   </html>
