46dd9500a8d51e7a22e1a210104c0cbe16012355
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2023-03-29 15:38:51 +0200

    Update framework2.blade.php

--- resources/views/tests/framework2.blade.php
+++ resources/views/tests/framework2.blade.php
@@ -3,16 +3,13 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Version 2</title>
+    <title>Version 3</title>
     <!-- Include Tailwind CSS -->
-    <link
-      rel="stylesheet"
-      href="https://unpkg.com/tailwindcss/dist/tailwind.min.css"
-    />
+    <script src="https://cdn.tailwindcss.com"></script>
   </head>
-  <body class="flex justify-center items-center h-screen border-4 border-red-500">
+<body class="flex justify-center items-center h-screen border-4 border-red-500">
     <div class="bg-red-500 rounded-lg p-8">
       <h1 class="text-white text-center text-4xl">Hello, world!</h1>
     </div>
   </body>
-</html>
+  </html>
