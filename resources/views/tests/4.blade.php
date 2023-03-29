40280e04758c04679fd798f8a6bf0f6478907b3a
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2023-03-29 22:30:58 +0200

    Update 4.blade.php

--- resources/views/tests/4.blade.php
+++ resources/views/tests/4.blade.php
@@ -14,18 +14,18 @@
 </head>
 <body class="flex flex-col h-screen">
     <header class="bg-blue-500 flex-none w-full" style="height: 15%;">
-        <div class="text-center text-white font-bold py-2">
+        <div class="text-center text-white font-bold py-2 flex items-center justify-center h-full">
             Header
         </div>
     </header>
     <main class="bg-green-500 flex-grow w-full overflow-y-auto">
-        <div class="text-center text-white font-bold py-2">
+        <div class="text-center text-white font-bold py-2 flex items-center justify-center h-full">
             Content
         </div>
         <!-- Add more content here to see the scrollable behavior -->
     </main>
     <footer class="bg-red-500 flex-none w-full" style="height: 5%;">
-        <div class="text-center text-white font-bold py-1">
+        <div class="text-center text-white font-bold py-1 flex items-center justify-center h-full">
             Footer
         </div>
     </footer>
