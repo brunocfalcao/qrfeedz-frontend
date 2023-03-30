2cb2ccd6d67f3e2ed2d2a9737897d35405d18feb
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2023-03-30 23:55:43 +0200

    Update 1.blade.php

--- resources/views/tests/progress/1.blade.php
+++ resources/views/tests/progress/1.blade.php
@@ -35,7 +35,7 @@
         </div>
     </main>
     <footer class="bg-red-500 flex-none w-full rounded-b-[2rem] mb-6 h-14">
-        <div class="text-center text-white font-bold py-1 flex items-center justify-between h-full px-[15%]">
+        <div class="text-white font-bold py-1 flex items-center justify-between h-full px-[15%]">
             <div class="-mt-12 flex flex-col items-center justify-center">
                 <div class="w-16 text-red-500">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 50">
@@ -51,7 +51,7 @@
                 <p class="text-xs pt-1">Home</p>
             </div>
 
-            <div class="text-red-400">
+            <div class="text-red-400 -ml-7">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
                   <path fill-rule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z" clip-rule="evenodd" />
                 </svg>
