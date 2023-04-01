4e080f4a85a5b9d3e67a64a0922577d85c4ebd8f
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2023-04-01 22:05:23 +0200

    wip

--- src/Views/Components/Support/ResponsiveBrackets.php
+++ src/Views/Components/Support/ResponsiveBrackets.php
@@ -13,6 +13,6 @@ class ResponsiveBrackets extends Component
 
     public function render()
     {
-        return view('qrfeedz::components.layout.responsive-brackets');
+        return view('qrfeedz::components.support.responsive-brackets');
     }
 }
